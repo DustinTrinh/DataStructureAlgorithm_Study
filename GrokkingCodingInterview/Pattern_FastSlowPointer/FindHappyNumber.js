37ded4dc703920612114a6dfe9f72664999b76e8
DustinTrinh <dustintrinh98@gmail.com>
2022-08-18 18:20:37 -0500

    Commit Grokking

---
+++ GrokkingCodingInterview/Pattern_FastSlowPointer/FindHappyNumber.js
@@ -0,0 +1,26 @@
+const find_happy_number = function (num) {
+    // TODO: Write your code here\
+    let slow = num;
+    let fast = num;
+
+    while (true) {
+        slow = calculatation(slow);
+        fast = calculatation(calculatation(fast));
+
+        if (slow === fast) break;
+    }
+    return slow === 1;
+};
+
+function calculatation(num) {
+    let sum = 0;
+    while (num > 0) {
+        let temp = num % 10;
+        sum += Math.pow(temp, 2);
+        num = Math.floor(num / 10);
+    }
+    return sum;
+}
+
+console.log(`${find_happy_number(23)}`);
+console.log(`${find_happy_number(12)}`);
---
+++ GrokkingCodingInterview/Pattern_TwoPointers/3SumSmallerThanK.js
@@ -0,0 +1,27 @@
+const triplet_with_smaller_sum = function (arr, target) {
+    count = 0;
+    arr.sort((a,b) => a-b);
+    console.log(arr)
+    for (let i = 0; i < arr.length; i++) {
+        let left = i + 1;
+        let right = arr.length - 1;
+
+        if (i > 0 && arr[i] === arr[i - 1]) continue;
+
+        while (left < right) {
+            let sum = arr[i] + arr[left] + arr[right];
+            
+            if (sum >= target) {
+                right--;
+            } else if (sum < target) {
+                count += right - left;
+                left++; 
+            }
+        }
+    }
+
+    return count;
+};
+
+//console.log(triplet_with_smaller_sum([-1, 0, 2, 3], 3));
+console.log(triplet_with_smaller_sum([-1, 4, 2, 1, 3], 5));
